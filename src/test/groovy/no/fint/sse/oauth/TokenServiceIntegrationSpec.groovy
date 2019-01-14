db55bc00971a8c7b31afa861ada6f148b9905349
Asgeir Nilsen <asgeir@fintlabs.no>
2019-01-14 22:39:34 +0100

    Updated specification.

--- src/test/groovy/no/fint/sse/oauth/TokenServiceIntegrationSpec.groovy
+++ src/test/groovy/no/fint/sse/oauth/TokenServiceIntegrationSpec.groovy
@@ -4,7 +4,6 @@ import no.fint.oauth.TokenService
 import no.fint.sse.testutils.TestSseServer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.web.client.RestTemplate
 import spock.lang.Specification
 
 @SpringBootTest(classes = TestSseServer)
@@ -13,17 +12,12 @@ class TokenServiceIntegrationSpec extends Specification {
     @Autowired(required = false)
     private TokenService tokenService
 
-    @Autowired
-    private RestTemplate restTemplate
-
-    def "Disable TokenService and return standard RestTemplate when fint.oauth.enabled is set to false"() {
+    def "Disable TokenService when fint.oauth.enabled is set to false"() {
         when:
         def tokenServicedisabled = (tokenService == null)
-        def restTemplateEnabled = (restTemplate != null)
 
         then:
         tokenServicedisabled
-        restTemplateEnabled
     }
 
 }
