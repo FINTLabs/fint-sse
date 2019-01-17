ab9045050e8e2c01793165a29cabc38a01b96f8a
Asgeir Nilsen <asgeir@fintlabs.no>
2019-01-17 20:12:56 +0100

    Removed OAuth classes, included from library instead.

--- src/test/groovy/no/fint/sse/testutils/TestSseServer.java
+++ src/test/groovy/no/fint/sse/testutils/TestSseServer.java
@@ -1,7 +1,7 @@
 package no.fint.sse.testutils;
 
 import lombok.extern.slf4j.Slf4j;
-import no.fint.sse.oauth.OAuthConfig;
+import no.fint.oauth.OAuthConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
